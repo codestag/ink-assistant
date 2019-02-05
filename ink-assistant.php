9d1ed6691376be67545f8695ea69d83e37df6de5
Krishna Kant <lushkant@gmail.com>
2019-02-05 17:01:55 +0530

    Add text domain for plugin translation

--- ink-assistant.php
+++ ink-assistant.php
@@ -6,6 +6,7 @@
  * Author: Codestag
  * Author URI: https://codestag.com
  * Version: 1.0
+ * Text Domain: ink-assistant
  * License: GPL2+
  * License URI: https://www.gnu.org/licenses/gpl-2.0.txt
  *
