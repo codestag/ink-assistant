cca08bc79d6dc128b9420d46e547175f09f2cf11
Krishna Kant <lushkant@gmail.com>
2019-02-11 15:25:16 +0530

    Add Template Loader Class

--- ink-assistant.php
+++ ink-assistant.php
@@ -89,6 +89,8 @@ if ( ! class_exists( 'Ink_Assistant' ) ) :
 			require_once IA_PLUGIN_PATH . 'includes/widgets/section-featured-slides.php';
 			require_once IA_PLUGIN_PATH . 'includes/widgets/static-content.php';
 			require_once IA_PLUGIN_PATH . 'includes/class-page-templater.php';
+			require_once IA_PLUGIN_PATH . 'includes/class-gamajo-template-loader.php';
+			require_once IA_PLUGIN_PATH . 'includes/class-ink-template-loader.php';
 			require_once IA_PLUGIN_PATH . 'includes/updater/updater.php';
 
 			if ( is_admin() ) : // Admin includes.
