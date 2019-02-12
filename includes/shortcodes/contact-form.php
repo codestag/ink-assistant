56e582cfe38b61c942f4ac3f7481b4f888534a61
Krishna Kant <lushkant@gmail.com>
2019-02-12 13:53:20 +0530

    Fix variable naming convention and standards formatting

--- includes/shortcodes/contact-form.php
+++ includes/shortcodes/contact-form.php
@@ -10,44 +10,44 @@
 
 function ink_contact_form_sc() {
 
-	$nameError         = __( 'Please enter your name.', 'ink' );
-	$emailError        = __( 'Please enter your email address.', 'ink' );
-	$emailInvalidError = __( 'You entered an invalid email address.', 'ink' );
-	$commentError      = __( 'Please enter a message.', 'ink' );
+	$name_error          = __( 'Please enter your name.', 'ink-assistant' );
+	$email_error         = __( 'Please enter your email address.', 'ink-assistant' );
+	$email_invalid_error = __( 'You entered an invalid email address.', 'ink-assistant' );
+	$comment_error       = __( 'Please enter a message.', 'ink-assistant' );
 
-	$errorMessages = array();
+	$error_messages = array();
 
 	if ( isset( $_POST['submitted'] ) ) {
 
 		if ( trim( $_POST['contactName'] ) === '' ) {
-			$errorMessages['nameError'] = $nameError;
-			$hasError                   = true;
+			$error_messages['nameError'] = $name_error;
+			$has_error                   = true;
 		} else {
 			$name = trim( $_POST['contactName'] );
 		}
 
 		if ( trim( $_POST['email'] ) === '' ) {
-			$errorMessages['emailError'] = $emailError;
-			$hasError                    = true;
+			$error_messages['emailError'] = $email_error;
+			$has_error                    = true;
 		} elseif ( ! is_email( trim( $_POST['email'] ) ) ) {
-			$errorMessages['emailInvalidError'] = $emailInvalidError;
-			$hasError                           = true;
+			$error_messages['emailInvalidError'] = $email_invalid_error;
+			$has_error                           = true;
 		} else {
 			$email = trim( $_POST['email'] );
 		}
 
 		if ( trim( $_POST['comments'] ) === '' ) {
-			$errorMessages['commentError'] = $commentError;
-			$hasError                      = true;
+			$error_messages['commentError'] = $comment_error;
+			$has_error                      = true;
 		} else {
 			$comments = stripslashes( trim( $_POST['comments'] ) );
 		}
 
-		if ( ! isset( $hasError ) ) {
-			$emailTo = stag_theme_mod( 'general_settings', 'contact_email' );
+		if ( ! isset( $has_error ) ) {
+			$email_to = stag_theme_mod( 'general_settings', 'contact_email' );
 
-			if ( ! isset( $emailTo ) || ( $emailTo == '' ) ) {
-				$emailTo = get_option( 'admin_email' );
+			if ( ! isset( $email_to ) || ( $email_to == '' ) ) {
+				$email_to = get_option( 'admin_email' );
 			}
 
 			$subject = '[Contact Form] From ' . $name;
@@ -57,72 +57,72 @@ function ink_contact_form_sc() {
 			$body   .= home_url();
 			$headers = 'From: ' . $name . ' <' . $email . '>' . "\r\n" . 'Reply-To: ' . $email;
 
-			wp_mail( $emailTo, $subject, $body, $headers );
-			$emailSent = true;
+			wp_mail( $email_to, $subject, $body, $headers );
+			$email_sent = true;
 		}
 	}
 
 	?>
 
 	<section class="ink-contact-form">
-		<?php if ( isset( $emailSent ) && true == $emailSent ) : ?>
+		<?php if ( isset( $email_sent ) && true === $email_sent ) : ?>
 
 		<div class="stag-alert stag-alert--green">
-			<p><?php _e( 'Thanks, your email was sent successfully.', 'ink' ); ?></p>
+			<p><?php esc_html_e( 'Thanks, your email was sent successfully.', 'ink-assistant' ); ?></p>
 		</div>
 
 		<?php else : ?>
 
 		<form action="<?php the_permalink(); ?>" id="contactForm" method="post">
-			<h2 class="ink-contact-form__title"><?php _e( 'Send a Message', 'ink' ); ?></h2>
+			<h2 class="ink-contact-form__title"><?php esc_html_e( 'Send a Message', 'ink-assistant' ); ?></h2>
 
 			<div class="grid">
 				<p class="unit one-of-two">
-					<label for="contactName"><?php _e( 'Name', 'ink' ); ?></label>
+					<label for="contactName"><?php esc_html_e( 'Name', 'ink-assistant' ); ?></label>
 					<input type="text" name="contactName" id="contactName" value="
 					<?php
 					if ( isset( $_POST['contactName'] ) ) {
 						echo esc_attr( $_POST['contactName'] );}
 					?>
 					" required>
-					<?php if ( isset( $errorMessages['nameError'] ) ) { ?>
-						<span class="error"><?php echo esc_html( $errorMessages['nameError'] ); ?></span>
+					<?php if ( isset( $error_messages['nameError'] ) ) { ?>
+						<span class="error"><?php echo esc_html( $error_messages['nameError'] ); ?></span>
 					<?php } ?>
 				</p>
 
 				<p class="unit one-of-two">
-					<label for="email"><?php _e( 'Email', 'ink' ); ?></label>
+					<label for="email"><?php esc_html_e( 'Email', 'ink-assistant' ); ?></label>
 					<input type="email" name="email" id="email" value="
 					<?php
 					if ( isset( $_POST['email'] ) ) {
-						echo esc_attr( $_POST['email'] );}
+						echo esc_attr( $_POST['email'] ); }
 					?>
 					" required>
-					<?php if ( isset( $errorMessages['emailError'] ) ) { ?>
-						<span class="error"><?php echo esc_html( $errorMessages['emailError'] ); ?></span>
+					<?php if ( isset( $error_messages['emailError'] ) ) { ?>
+						<span class="error"><?php echo esc_html( $error_messages['emailError'] ); ?></span>
 					<?php } ?>
-					<?php if ( isset( $errorMessages['emailInvalidError'] ) ) { ?>
-						<span class="error"><?php echo esc_html( $errorMessages['emailInvalidError'] ); ?></span>
+					<?php if ( isset( $error_messages['emailInvalidError'] ) ) { ?>
+						<span class="error"><?php echo esc_html( $error_messages['emailInvalidError'] ); ?></span>
 					<?php } ?>
 				</p>
 
 				<p class="unit span-grid">
-					<label for="commentsText"><?php _e( 'Your Message', 'ink' ); ?></label>
+					<label for="commentsText"><?php esc_html_e( 'Your Message', 'ink-assistant' ); ?></label>
 					<textarea rows="6" name="comments" id="commentsText" required>
 					<?php
 					if ( isset( $_POST['comments'] ) ) {
 						echo stripslashes( $_POST['comments'] ); }
 					?>
 					</textarea>
-					<?php if ( isset( $errorMessages['commentError'] ) ) { ?>
-						<span class="error"><?php echo esc_html( $errorMessages['commentError'] ); ?></span>
+					<?php if ( isset( $error_messages['commentError'] ) ) { ?>
+						<span class="error"><?php echo esc_html( $error_messages['commentError'] ); ?></span>
 					<?php } ?>
 				</p>
 
 			</div>
 
 			<p class="buttons">
-				<input type="submit" id="submitted" class="contact-form-button" name="submitted" value="<?php esc_attr_e( 'Send Message', 'ink' ); ?>">
+				<input type="submit" id="submitted" class="contact-form-button" name="submitted" value="<?php esc_attr_e( 'Send Message', 'ink-assistant' ); ?>">
 			</p>
 
 		</form>
